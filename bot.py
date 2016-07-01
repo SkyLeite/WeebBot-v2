c788ccfd744f59da5d0534788e0f743f6fb1bf6e
Kaze Senoue <kazesenoue@gmail.com>
2016-07-01 16:23:29 -0300

    Added logging functionality and improved EQ alerts
    Also 4 > 2

--- bot.py
+++ bot.py
@@ -1,113 +1,137 @@
-import discord, asyncio, random, requests, json, re, random
 from functions import *
 from settings import *
 
 client = discord.Client()
 
 print("Starting bot...\n")
-  
+
+
 @client.event
 async def on_ready():
-  print("## BOT IS RUNNING ##")
-  print('Username: %s' % client.user.name)
-  print('ID: %s' % client.user.id)
-  print('------')
-  
+    print("## BOT IS RUNNING ##")
+    print('Username: %s' % client.user.name)
+    print('ID: %s' % client.user.id)
+    print('------')
+
+
 @client.event
 async def on_message(message):
-  if message.content.startswith('!cat'):
-    await showCat(message, client)
-   
-  elif message.content.startswith('!ping'):
-    await ping(message, client)
-  
-  elif message.content.startswith('!id'):
-    await client.send_message(message.channel, message.channel.id)
-    
-  elif message.content.startswith('!send'):
-    print(message.attachments)
-  
-  elif message.content.startswith('!meme'):
-    msg = message.content.split(' ')
-    
-    if msg[1] == "play":
-      meme = msg[2] + '.mp3'
-      await playMeme(message, client, meme)
-    
-    elif msg[1] == "list":
-      await memeList(message, client)
-      
-    elif msg[1] == "add":
-      await addMeme(message, client)
-      
-  elif 'ayy' in message.content:
-    await client.send_message(message.channel, 'lmao')
-    
-  elif message.content.startswith('!recommend'):
-    query = message.content.split(' ',1)[1]
-    await showRecommendation(message, client, query)
-    
-  elif message.content.startswith('!number'):
-    await showNumberTrivia(message, client)
-    
-  elif message.content.startswith('!pokemon'):
-    pokemon = message.content.split(' ',1)[1]
-    await showPokemon(message, client, pokemon)
-    
-  elif message.content.startswith('!msg'):
-    if message.author.id == ownerid:
-      message = message.content.split(' ',1)[1]
-      
-      for item in client.servers:
-        channel = discord.Object(item.id)
-        await client.send_message(channel, message)
-  
-  elif message.content.startswith('!join'):
-    await client.send_message(message.channel, 'To get Weeb Bot in your server, simply click the following link: http://bit.ly/1X4p8U3')
-    
-  elif message.content.startswith('!servers'):
-    for item in client.servers:
-      print(item)
-      
-  elif message.content.startswith('!game'):
-    if message.author.id == ownerid:
-      game = message.content.split(' ',1)[1]
-      await client.change_status(message.author.game, idle=False)
-    else:
-      await client.send_message(message.channel, 'Your ID:%s \nOwner ID:%s' % (message.author.id, ownerid))
+    if message.content.startswith('!cat'):
+        await showCat(message, client)
 
-  elif message.content.startswith('!eq'):
-    message2 = message.content.split(" ")
-    command = message2[1]
-    admin = 'False'
-    if command == 'enable':
-      for item in message.author.roles:
-        if item.name == "Administrator":
-          await addEQChannel(message, client)
-          admin = 'True'
-      if admin == 'False':
-        await client.send_message(message.channel, "You need the 'Administrator' role to do that.")
-    elif command == 'disable':
-      for item in message.author.roles:
-        if item.name == "Administrator":
-          await removeEQChannel(message, client)
-          admin = 'True'
-      if admin == 'False':
-        await client.send_message(message.channel, "You need the 'Administrator' role to do that.")
-    else:
-      await client.send_message(message.channel, "Welcome to Weeb Bot's Emergency Quest Alert! To get started, please type !eq enable on the channel you want EQs to appear. Please keep in mind you need the 'Administrator' role to do that.")
-  
+    elif message.content.startswith('!ping'):
+        await ping(message, client)
+
+    elif message.content.startswith('!id'):
+        await client.send_message(message.channel, message.channel.id)
+
+    elif message.content.startswith('!send'):
+        print(message.attachments)
+
+    elif message.content.startswith('!meme'):
+        msg = message.content.split(' ')
+
+        if msg[1] == "play":
+            meme = msg[2] + '.mp3'
+            await playMeme(message, client, meme)
+
+        elif msg[1] == "bye":
+            meme = 'bye'
+            await playMeme(message, client, meme)
+
+        elif msg[1] == "list":
+            await memeList(message, client)
+
+        elif msg[1] == "add":
+            if message.author.id == ownerid:
+                await addMeme(message, client)
+
+    elif 'ayy' in message.content and 'lmao' not in message.content:
+        await client.send_message(message.channel, 'lmao')
+
+    elif message.content.startswith('!recommend'):
+        query = message.content.split(' ', 1)[1]
+        await showRecommendation(message, client, query)
+
+    elif message.content.startswith('!pokemon'):
+        pokemon = message.content.split(' ', 1)[1]
+        await showPokemon(message, client, pokemon)
+
+    elif message.content.startswith('!lasteq'):
+        await showLastEQ(client, message)
+
+    elif message.content.startswith('!msg'):
+        if message.author.id == ownerid:
+            message = message.content.split(' ', 1)[1]
+
+            try:
+                for item in client.servers:
+                    await client.send_message(client.servers.default_channel, message)
+            except:
+                pass
+
+    elif message.content.startswith('!join'):
+        await client.send_message(message.channel,
+                                  'To get Weeb Bot in your server, simply click the following link: http://bit.ly/1X4p8U3')
+
+    elif message.content.startswith('!servers'):
+        servers = []
+
+        for item in client.servers:
+            servers.append('%s(%d)' % (item.name, item.member_count))
+
+        string = ', '.join(servers)
+        await client.send_message(message.channel, 'Servers: %s' % string)
+
+    elif message.content.lower() == 'i see what you did there':
+        await client.send_file(message.channel, 'img/isee.jpg')
+
+    elif message.content.startswith('!game'):
+        if message.author.id == ownerid:
+            gamename = message.content.split(' ', 1)[1]
+
+            game = discord.Game(name=gamename)
+            await client.change_status(game, idle=False)
+        else:
+            await client.send_message(message.channel, 'Your ID:%s \nOwner ID:%s' % (message.author.id, ownerid))
+
+    elif message.content.startswith('!eq'):
+        message2 = message.content.split(" ")
+        command = message2[1]
+        admin = 'False'
+        if command == 'enable':
+            for item in message.author.roles:
+                if item.name == "Administrator":
+                    await addEQChannel(message, client)
+                    admin = 'True'
+            if admin == 'False':
+                await client.send_message(message.channel, "You need the 'Administrator' role to do that.")
+        elif command == 'disable':
+            for item in message.author.roles:
+                if item.name == "Administrator":
+                    await removeEQChannel(message, client)
+                    admin = 'True'
+            if admin == 'False':
+                await client.send_message(message.channel, "You need the 'Administrator' role to do that.")
+        else:
+            await client.send_message(message.channel,
+                                      "Welcome to Weeb Bot's Emergency Quest Alert! To get started, please type !eq enable on the channel you want EQs to appear. Please keep in mind you need the 'Administrator' role to do that.")
+
+
 @client.event
 async def on_server_join(server):
-  await client.send_message(server, 'Ohayo!')
-  
+    channel = discord.Object(test_channel)
+    await client.send_message(channel, u':mega: **Joined:** {0:s}\n``Member Count:`` {1:s}\n``Icon URL:`` {2:s}'.format(
+      server.name, server.member_count, server.icon_url))
+
+
 loop = asyncio.get_event_loop()
-  
+
 try:
-  loop.create_task(showPSO2EQ(client))
-  loop.run_until_complete(client.login(token))
-  loop.run_until_complete(client.connect())
+    loop.create_task(showPSO2EQ(client))
+    loop.run_until_complete(client.login(token))
+    loop.run_until_complete(client.connect())
 except Exception:
-  loop.run_until_complete(client.close())
+    loop.run_until_complete(client.close())
 finally:
-  loop.close()
+    loop.close()
--- functions.py
+++ functions.py
@@ -1,210 +1,288 @@
-import discord, asyncio, random, requests, json, re, random, os
-from settings import *
+import asyncio
+import discord
+import json
+import os
+import random
+import re
+import requests
+import urllib.request
+
 global loop
 
 loop = asyncio.get_event_loop()
 
+
 async def playMeme(message, client, meme):
-  if message.author.voice_channel is not None:
-    channel = message.author.voice_channel
-    file = "audio/%s" % meme
-    meme = re.sub('\.mp3$', '', meme)
-    
-    voice = await client.join_voice_channel(channel)
-    player = voice.create_ffmpeg_player(file, after=lambda: disconnect(client, voice))
+    if meme == 'bye':
+        channel = message.author.voice_channel
+        voice = discord.VoiceClient()
+        await voice.disconnect()
+    else:
+        if message.author.voice_channel is not None:
+            channel = message.author.voice_channel
+            file = "audio/%s" % meme
+            meme = re.sub('\.mp3$', '', meme)
 
-    player.start()
-    
-    await client.send_message(message.channel, 'Playing ``%s`` in ``%s``' % (meme, message.author.voice_channel.name))
-  else:
-    await client.send_message(message.channel, 'You are not in a voice channel.')
-    
+            voice = await client.join_voice_channel(channel)
+            player = voice.create_ffmpeg_player(file, after=lambda: disconnect(client, voice))
+
+            player.start()
+
+            await client.send_message(message.channel,
+                                      'Playing ``%s`` in ``%s``' % (meme, message.author.voice_channel.name))
+        else:
+            await client.send_message(message.channel, 'You are not in a voice channel.')
+
+
 def disconnect(client, voice):
-  coro = voice.disconnect()
-  fut = asyncio.run_coroutine_threadsafe(coro, client.loop)
+    coro = voice.disconnect()
+    fut = asyncio.run_coroutine_threadsafe(coro, client.loop)
 
-  fut.result()
-    
+    fut.result()
+
+
+async def addMeme(message, client):
+    array = message.content.split(' ')
+
+    try:
+        name = array[2]
+        url = array[3]
+
+        urllib.request.urlretrieve(url, 'audio/%s.mp3' % name)
+        await client.send_message(message.channel, '``%s`` successfully added.' % name)
+    except:
+        await client.send_message(message.channel, 'Something went wrong. Check your syntax, or try another filehost.')
+
+
 async def memeList(message, client):
-  indir = 'audio/'
-  memes = []
-    
-  for root, dirs, filenames in os.walk(indir):
-    for f in filenames:
-      j = re.sub('\.mp3$', '', f)
-      memes.append(j)
-  
-  string = ', '.join(memes)
-  await client.send_message(message.channel, 'List of available dank maymays:\n %s' % string)
+    indir = 'audio/'
+    memes = []
+
+    for root, dirs, filenames in os.walk(indir):
+        for f in filenames:
+            j = re.sub('\.mp3$', '', f)
+            k = '``%s``' % j
+            memes.append(k)
+
+    string = ', '.join(memes)
+    await client.send_message(message.channel, '**List of available dank maymays:** \n\n%s' % string)
+
 
 async def showCat(message, client):
-  offset = random.randint(0,348)
-  
-  r = requests.get("https://montanaflynn-cat-overflow.p.mashape.com/?limit=1&offset=%i" % offset,
-  headers={
-  "X-Mashape-Key": "pzH8A2js8xmshCoMqkJkXSCGxzz9p1p1Ue9jsndowy6k3oVmhw",
-  "Accept": "text/plain"
-  }
-  )
-  
-  url = str(r.text)
-  url2 = url.replace('\n', '')
+    offset = random.randint(0, 348)
 
-  gif = url2.split('/')
-  gifid = gif[3]
-  gif2 = gifid.replace('c', '', 1)
-  
-  r = requests.get("http://catfacts-api.appspot.com/api/facts?number=1")
-  response = json.loads(r.text)
-  
-  fact = response['facts'][0]
-  
-  await client.send_message(message.channel, 'http://catoverflow.com/cats/%s.gif \n\nRandom fact: %s' % (gif2, fact))
-  
-async def showPokemon(message, client, pokemon):
-  try:
-    api = "http://pokeapi.co/api/v2/pokemon/%s/" % pokemon.lower()
-    
-    future1 = loop.run_in_executor(None, requests.get, api)
-    r = await future1
-    
+    r = requests.get("https://montanaflynn-cat-overflow.p.mashape.com/?limit=1&offset=%i" % offset,
+                     headers={
+                         "X-Mashape-Key": "pzH8A2js8xmshCoMqkJkXSCGxzz9p1p1Ue9jsndowy6k3oVmhw",
+                         "Accept": "text/plain"
+                     }
+                     )
+
+    url = str(r.text)
+    url2 = url.replace('\n', '')
+
+    gif = url2.split('/')
+    gifid = gif[3]
+    gif2 = gifid.replace('c', '', 1)
+
+    r = requests.get("http://catfacts-api.appspot.com/api/facts?number=1")
     response = json.loads(r.text)
-    abilities = []
-    types = []
-    
-    #Assings types and abilities to lists
-    for i in response['abilities']:
-      abilities.append(i['ability']['name'])
-    
-    for i in response['types']:
-      types.append(i['type']['name'])
-      
-    #Converts lists to human readable strings
-    ability = ', '.join(abilities)
-    type = ', '.join(types)
-    
-    #Assigns data to variables for easier reading
-    name = response['name']
-    weight = response['weight']
-    height = response['height']
-    speed = response['stats'][0]['base_stat']
-    spdef = response['stats'][1]['base_stat']
-    spatk = response['stats'][2]['base_stat']
-    defense = response['stats'][3]['base_stat']
-    attack = response['stats'][4]['base_stat']
-    hp = response['stats'][5]['base_stat']
-    sprite = response['sprites']['front_default']
-    shiny = response['sprites']['front_shiny']
-    
-    string = '**Name:** %s\n**Type:** %s\n**Weight:** %s\n**Height:** %s\n**Abilities:** %s\n**Stats:**\n  Speed: %s\n  Special Defense: %s\n  Special Attack: %s\n  Defense: %s\n  Attack: %s\n  HP: %s\n**Sprite (Normal):** %s\n**Sprite (Shiny):** %s' % (name, type, weight, height, ability, speed, spdef, spatk, defense, attack, hp, sprite, shiny)
-    await client.send_message(message.channel, string)
-  except:
-    await client.send_message(message.channel, 'Could not find a Pokémon with the name ``%s``' % pokemon)
-  
+
+    fact = response['facts'][0]
+
+    await client.send_message(message.channel, 'http://catoverflow.com/cats/%s.gif \n\nRandom fact: %s' % (gif2, fact))
+
+async def showPokemon(message, client, pokemon):
+    try:
+        api = "http://pokeapi.co/api/v2/pokemon/%s/" % pokemon.lower()
+
+        future1 = loop.run_in_executor(None, requests.get, api)
+        r = await future1
+
+        response = json.loads(r.text)
+        abilities = []
+        types = []
+
+        # Assings types and abilities to lists
+        for i in response['abilities']:
+            abilities.append(i['ability']['name'])
+
+        for i in response['types']:
+            types.append(i['type']['name'])
+
+        # Converts lists to human readable strings
+        ability = ', '.join(abilities)
+        type = ', '.join(types)
+
+        # Assigns data to variables for easier reading
+        name = response['name']
+        weight = response['weight']
+        height = response['height']
+        speed = response['stats'][0]['base_stat']
+        spdef = response['stats'][1]['base_stat']
+        spatk = response['stats'][2]['base_stat']
+        defense = response['stats'][3]['base_stat']
+        attack = response['stats'][4]['base_stat']
+        hp = response['stats'][5]['base_stat']
+        sprite = response['sprites']['front_default']
+        shiny = response['sprites']['front_shiny']
+
+        string = '**Name:** %s\n**Type:** %s\n**Weight:** %s\n**Height:** %s\n**Abilities:** %s\n**Stats:**\n  Speed: %s\n  Special Defense: %s\n  Special Attack: %s\n  Defense: %s\n  Attack: %s\n  HP: %s\n**Sprite (Normal):** %s\n**Sprite (Shiny):** %s' % (
+        name, type, weight, height, ability, speed, spdef, spatk, defense, attack, hp, sprite, shiny)
+        await client.send_message(message.channel, string)
+    except:
+        await client.send_message(message.channel, 'Could not find a Pokémon with the name ``%s``' % pokemon)
+
+
 async def showNumberTrivia(message, client):
-  r = requests.get("https://numbersapi.p.mashape.com/random/trivia?json=true",
-      headers={
-        "X-Mashape-Key": "Q7n8VApOzPmshmeZQNtthye630hmp1LDw0HjsnZIoR8eOu4JX6",
-        "Accept": "text/plain"
-    }
-  )
-  
-  response = json.loads(r.text)
-  await client.send_message(message.channel, response['text'])
-    
+    r = requests.get("https://numbersapi.p.mashape.com/random/trivia?json=true",
+                     headers={
+                         "X-Mashape-Key": "Q7n8VApOzPmshmeZQNtthye630hmp1LDw0HjsnZIoR8eOu4JX6",
+                         "Accept": "text/plain"
+                     }
+                     )
+
+    response = json.loads(r.text)
+    await client.send_message(message.channel, response['text'])
+
+
 async def ping(message, client):
-  await client.send_message(message.channel, 'Pong!')
- 
+    await client.send_message(message.channel, 'Pong!')
+
+
 async def showPSO2EQ(client):
-  while not client.is_closed:
-    #Async shit to use requests
-    future1 = loop.run_in_executor(None, requests.get, 'http://pso2emq.flyergo.eu/api/v2/')
-    r = await future1
-    
-    #Loads EQ data
-    eq = json.loads(r.text)
-    
-    #Loads last_eq.json
-    with open('last_eq.json', encoding="utf8") as in_f:
-      last_eq = json.load(in_f)
-    
-    #If current EQ is different than last EQ recorded, send alert and update last_eq file
-    if last_eq['jst'] != eq[0]['jst']:
-      with open('eq_channels.json', encoding="utf8") as eq_channels:
-        eq_channels = json.load(eq_channels)
-      
-      try:
-        for item in eq_channels['channels']:
-          channel = discord.Object(item)
-          await client.send_message(channel, eq[0]['text'])
-        print('#EQ ALERT!')
-      except:
+    while not client.is_closed:
+        # Async shit to use requests
+        future1 = loop.run_in_executor(None, requests.get, 'http://pso2emq.flyergo.eu/api/v2/')
+        r = await future1
+
+        # Loads EQ data
+        r2 = json.loads(r.text)
+        eq = r2[0]['text'].splitlines()
+        eqtime = r2[0]['jst']
+        eqs = []
+        i = 0
+
+        # Adds EQ data to eqs and formats them properly
+        for line in eq:
+            if 'Emergency Quest' not in line and line != 'Ship%02d: -' % i and line.startswith('Ship'):
+                line = '``' + line.replace(':', ':``')
+                line = line.replace('Ship', 'SHIP ')
+                eqs.append(line)
+
+            if line == 'All ships are in event preparation.':
+                eqs.append('``' + line + '``')
+
+            if line.startswith('[[In Progress]'):
+                line = line.replace('[In Progress]', '``IN PROGRESS:``')
+                eqs.append(line)
+
+            if line.startswith('[In Preparation]'):
+                line = line.replace('[In Preparation]', '``IN 1 HOUR:``')
+                eqs.append(line)
+
+            if line.startswith('[1 hour later]'):
+                line = line.replace('[1 hour later]', '``IN 2 HOURS:``')
+                eqs.append(line)
+
+            if line.startswith('[2 hours later]'):
+                line = line.replace('[2 hours later]', '``IN 3 HOURS:``')
+                eqs.append(line)
+
+            i = i + 1
+
+        # Loads last_eq.json
+        with open('last_eq.json', encoding="utf8") as in_f:
+            last_eq = json.load(in_f)
+
+        # If current EQ is different than last EQ recorded, send alert and update last_eq file
+        if last_eq['jst'] != eqtime:
+            with open('eq_channels.json', encoding="utf8") as eq_channels:
+                eq_channels = json.load(eq_channels)
+
+            try:
+                string = '\n'.join(eqs)
+                message = ':mega: **%s JST Emergency Quest Notice**\n\n%s' % (eqtime, string)
+                for item in eq_channels['channels']:
+                    channel = discord.Object(item)
+                    await client.send_message(channel, message)
+            except:
+                pass
+
+            with open('last_eq.json', 'w') as out_f:
+                json.dump(r2[0], out_f)
+
+        await asyncio.sleep(300)  # Task runs every 300 seconds
+
+
+async def showLastEQ(client, message):
+    eqs = []
+
+    with open('last_eq.json', 'r') as file:
+        eq = json.load(file)
+
+    eqtime = eq['jst']
+    i = 0
+
+    # Adds EQ data to eqs and formats them properly
+    for line in eq['text'].splitlines():
+        if 'Emergency Quest' not in line and line != 'Ship%02d: -' % i and line.startswith('Ship'):
+            line = '``' + line.replace(':', ':``')
+            line = line.replace('Ship', 'SHIP ')
+            eqs.append(line)
+
+        if line == 'All ships are in event preparation.':
+            eqs.append('``' + line + '``')
+
+        if line.startswith('[[In Progress]'):
+            line = line.replace('[In Progress]', '``IN PROGRESS:``')
+            eqs.append(line)
+
+        if line.startswith('[In Preparation]'):
+            line = line.replace('[In Preparation]', '``IN 1 HOUR:``')
+            eqs.append(line)
+
+        if line.startswith('[1 hour later]'):
+            line = line.replace('[1 hour later]', '``IN 2 HOURS:``')
+            eqs.append(line)
+
+        if line.startswith('[2 hours later]'):
+            line = line.replace('[2 hours later]', '``IN 3 HOURS:``')
+            eqs.append(line)
+
+        i = i + 1
+
+    try:
+        string = '\n'.join(eqs)
+        lasteq = ':mega: **%02d JST Emergency Quest Notice**\n\n%s' % (eqtime, string)
+        await client.send_message(message.channel, lasteq)
+    except:
         pass
-    
-      with open('last_eq.json', 'w') as out_f:
-        json.dump(eq[0], out_f)
-      
-    await asyncio.sleep(600) #Task runs every 60 seconds
-    
+
+
 async def addEQChannel(message, client):
-  #Loads eq_channels.json file
-  with open('eq_channels.json', encoding="utf8") as eq_channels:
-    eq_channels = json.load(eq_channels)
-    
-  if message.channel.id not in eq_channels['channels']:
-    #Writes channel ID to file
-    with open('eq_channels.json', 'w') as outfile:
-      eq_channels['channels'].append(message.channel.id)
-      json.dump(eq_channels, outfile)
-      
-    await client.send_message(message.channel, "EQ Alerts successfully enabled on this channel.")
-  else:
-    await client.send_message(message.channel, 'EQ Alerts are already enabled on this channel.')
-    
-async def removeEQChannel(message, client):
-    #Loads eq_channels.json file
+    # Loads eq_channels.json file
     with open('eq_channels.json', encoding="utf8") as eq_channels:
-      eq_channels = json.load(eq_channels)
-      eq_channels['channels'].remove(message.channel.id)
-      
-    #Writes channel ID to file
+        eq_channels = json.load(eq_channels)
+
+    if message.channel.id not in eq_channels['channels']:
+        # Writes channel ID to file
+        with open('eq_channels.json', 'w') as outfile:
+            eq_channels['channels'].append(message.channel.id)
+            json.dump(eq_channels, outfile)
+
+        await client.send_message(message.channel, "EQ Alerts successfully enabled on this channel.")
+    else:
+        await client.send_message(message.channel, 'EQ Alerts are already enabled on this channel.')
+
+
+async def removeEQChannel(message, client):
+    # Loads eq_channels.json file
+    with open('eq_channels.json', encoding="utf8") as eq_channels:
+        eq_channels = json.load(eq_channels)
+        eq_channels['channels'].remove(message.channel.id)
+
+    # Writes channel ID to file
     with open('eq_channels.json', 'w') as outfile:
-      json.dump(eq_channels, outfile)
+        json.dump(eq_channels, outfile)
     await client.send_message(message.channel, 'EQ Alerts successfully disabled on this channel.')
-    
-def fixFormat(stats, name):
-  #Returns rarity
-  m = re.search('\d\d', stats[0])
-  print(stats[0])
-  if m:
-    rarity = m.group(0)
-  
-  #Returns name
-  m = re.search('\[\[%s\]\]' % name, stats[2])
-  if m:
-    weapon = m.group(0)
-    weapon = weapon.replace('[', '')
-    weapon = weapon.replace(']', '')
-  
-    #Returns requirement
-    #m = re.search('\d\d\d\s\w\-ATK', stats[3])
-    #if m:
-    #  req = m.group(0)
-
-    #Returns base
-    m = stats[5].split('|')
-    m = m[1].split('<')
-    base = m[0]
-  
-    #Returns +10
-    m = stats[6].split('|')
-    m = m[1].split('<')
-    grinded = m[0]
-  
-    #Returns pot
-    m = re.search('\{\{pots\|.*\}\}', stats[7])
-    if m:
-      pot = m.group(0).split('|')
-      pot = pot[1].split('}')
-
-  return(rarity, weapon, base, grinded, pot[0])
