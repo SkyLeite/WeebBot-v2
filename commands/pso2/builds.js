fb085f75b4e5bf456f5d0fb7641ac9aff3f254ab
KazeSenoue <kazesenoue@gmail.com>
2017-02-08 16:12:30 -0200

    Catching exceptions on setInterval

--- commands/pso2/builds.js
+++ commands/pso2/builds.js
@@ -16,17 +16,23 @@ module.exports = class PSO2Commands extends Commando.Command {
             color: 3447003,
             title: "Phantasy Star Online 2",
             url: "http://pso2.jp",
+            footer: {
+                text: "Subject to change."
+            },
             fields: [{
                 name: "Striking",
-                value: "[Hu/Fi](https://goo.gl/DZQb73)\n[Fi/Hu](https://goo.gl/fcJZpV)\n[Br/Hu](https://goo.gl/dZyF9X)\n[Bo/Hu](https://goo.gl/OZOMN7)"
+                value: "[Hu/Fi](https://goo.gl/DZQb73)\n[Fi/Hu](https://goo.gl/fcJZpV)\n[Br/Hu](https://goo.gl/dZyF9X)\n[Bo/Hu](https://goo.gl/OZOMN7)",
+                inline: true
             },
             {
                 name: "Ranged",
-                value: "[Ra/Hu](https://goo.gl/8O7Wms)\n[Br/Ra](https://goo.gl/SAaSA6)\n[Gu/Ra](https://goo.gl/mdNFR2)"
+                value: "[Ra/Hu](https://goo.gl/8O7Wms)\n[Br/Ra](https://goo.gl/SAaSA6)\n[Gu/Ra](https://goo.gl/mdNFR2)",
+                inline: true
             },
             {
                 name: "Tech",
-                value: "[Fo/Te](https://goo.gl/hQthMq)\n[Te/Br](https://goo.gl/bqpYJN)\n[Su/XX](https://goo.gl/AEIBzm)"
+                value: "[Fo/Te](https://goo.gl/hQthMq)\n[Te/Br](https://goo.gl/bqpYJN)\n[Su/XX](https://goo.gl/AEIBzm)",
+                inline: true
             }]
         }});
     }
