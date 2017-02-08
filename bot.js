66a1cd82305f64b67826693ac43d175be7c647e1
KazeSenoue <kazesenoue@gmail.com>
2017-02-08 15:15:10 -0200

    :D?

--- bot.js
+++ bot.js
@@ -69,9 +69,7 @@ client.setInterval(function() {
                                 });
 
                                 let string = `:arrow_right: **Emergency Quest Notice**\n\n:watch:**IN 40 MINUTES:**\n${format.join('\n')}\n\n:love_letter: Support the bot! http://kaze.rip/donate`
-                                if (client.channels.get(settings['channel'])){
-                                    client.channels.get(settings['channel']).sendMessage(string);
-                                }
+                                client.channels.get(settings['channel']).sendMessage(string).catch(console.log("hi"));
                             }
                         }
                     }
