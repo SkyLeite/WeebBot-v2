068ab021d8a06046288eba771f59ce0fbd6386c2
KazeSenoue <kazesenoue@gmail.com>
2017-06-09 19:43:40 -0300

    Rewritten `sv` command. Now works with own API, which is a lot faster than looping through the old API.

--- commands/card_games/sv.js
+++ commands/card_games/sv.js
@@ -21,62 +21,41 @@ module.exports = class SVCommands extends Commando.Command {
         })
     }
 
-    async run(msg, args, client){
+    async run(msg, args, client) {
         let card = args.card;
 
-        request('https://shadowverse-portal.com/api/v1/cards?format=json&lang=en', function(error, response, body) {
-            if (!error && response.statusCode == 200) { 
-                let data = JSON.parse(body);
-                var indice = 0;
-                var matches = [];
-                for (let i = 0; i < data['data']['cards'].length; i++){
-                    if (data['data']['cards'][i]['card_name'].toLowerCase().trim().includes(card.toLowerCase().trim())){
-                        if (data['data']['cards'][i]['card_name'].toLowerCase().trim() == card.toLowerCase().trim()) {
-                            matches = [data['data']['cards'][i]]; //Found an exact match.
-                            break;
+        request(`http://sv.kaze.rip/cards/${card}`, (error, response, body) => {
+            if (!error && response.statusCode == 200) {
+                const data = JSON.parse(body);
+                if (data.length > 0) {
+                    const cardInfo = data[0];
+                    return msg.reply("", {
+                        embed: {
+                            color: 3447003,
+                            title: `Shadowverse`,
+                            url: "https://shadowverse.com",
+                            thumbnail: {
+                                url: `https://shadowverse-portal.com/image/card/en/C_${cardInfo['card_id']}.png`
+                            },
+                            fields: [{
+                                name: "Stats",
+                                value: `**Name:** ${cardInfo['card_name']}\n**Cost:** ${cardInfo['cost']} PP\n**Attack / Defense:** ${cardInfo['atk']}/${cardInfo['life']}\n**Evo Attack / Defense:** ${cardInfo['evo_atk']}/${cardInfo['evo_life']}`,
+                                inline: true
+                            },
+                            {
+                                name: "Skills",
+                                value: `**Skill:** ${cardInfo['skill_disc']}\n**Evo Skill:** ${cardInfo['evo_skill_disc']}`
+                            },
+                            {
+                                name: "Lore",
+                                value: `**Description:** ${cardInfo['description']}\n**Evo Description:** ${cardInfo['evo_description']}\n**Tribe:** ${cardInfo['tribe_name']}`
+                            },
+                            {
+                                name: "Art",
+                                value: `[Classic](https://shadowverse-portal.com/image/card/en/C_${cardInfo['card_id']}.png) - [Evolved](https://shadowverse-portal.com/image/card/en/E_${cardInfo['card_id']}.png)`
+                            }]
                         }
-                        matches[indice++] = data['data']['cards'][i]; //Found a "close enough" match.
-                    }
-                }
-                if (matches.length > 1) { //i.e. more than 1 match.
-                    let max = 10;
-                    var answer = `\`${card}\` matched ${matches.length} cards : \n\`\`\``
-                    for(let i = 0; i < max && i < matches.length; i++) {
-                        answer += matches[i]['card_name'] + `\n`;
-                    }
-                    if(matches.length > max) {
-                        answer += `...and ${matches.length - max} more`
-                    }
-                    answer += `\`\`\``;
-                    return msg.reply(answer);
-                }
-                var cardInfo = matches[0]; //i.e. only 1 "close enough" match OR an exact match.
-                if (typeof cardInfo != 'undefined') {
-                    return msg.reply("", {embed: {
-                        color: 3447003,
-                        title: `Shadowverse`,
-                        url: "https://shadowverse.com",
-                        thumbnail: {
-                            url: `https://shadowverse-portal.com/image/card/en/C_${cardInfo['card_id']}.png`
-                        },
-                        fields: [{
-                            name: "Stats",
-                            value: `**Name:** ${cardInfo['card_name']}\n**Cost:** ${cardInfo['cost']} PP\n**Attack / Defense:** ${cardInfo['atk']}/${cardInfo['life']}\n**Evo Attack / Defense:** ${cardInfo['evo_atk']}/${cardInfo['evo_life']}`,
-                            inline: true
-                        },
-                        {
-                            name: "Skills",
-                            value: `**Skill:** ${cardInfo['skill_disc']}\n**Evo Skill:** ${cardInfo['evo_skill_disc']}`
-                        },
-                        {
-                            name: "Lore",
-                            value: `**Description:** ${cardInfo['description']}\n**Evo Description:** ${cardInfo['evo_description']}\n**Tribe:** ${cardInfo['tribe_name']}`
-                        },
-                        {
-                            name: "Art",
-                            value: `[Classic](https://shadowverse-portal.com/image/card/en/C_${cardInfo['card_id']}.png) - [Evolved](https://shadowverse-portal.com/image/card/en/E_${cardInfo['card_id']}.png)`
-                        }]
-                    }});
+                    });
                 }
                 else {
                     return msg.reply(`\`${card}\` did not match any cards. Please try again.`)
