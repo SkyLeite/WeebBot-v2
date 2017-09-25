65e71ec5dd19f3f4170075d4f463924bbd7b2a73
Rodrigo Fernandes <rodrigo.fernandes@uem.com.br>
2017-09-25 11:37:02 -0300

    Moved EQ handling to a separate file for organization

--- bot.js
+++ bot.js
@@ -5,6 +5,7 @@ const request = require('request');
 const fetch = require('node-fetch');
 const fs = require('mz/fs');
 const moment = require('moment');
+const handleEQs = require('./eqs.js');
 
 if (!fs.existsSync('./config.json')) {
     fs.writeFileSync('./config.json', '{"token" : "", "prefix" : "!"}')
@@ -56,52 +57,6 @@ client.setProvider(
 ).catch(console.error);
 
 // EQ alerts
-client.setInterval(async () => {
-    try {
-        const response = await fetch('http://pso2.kaze.rip/eq/');
-        if (response.status !== 200) return;
-
-        const data = await response.json();
-        const cache = JSON.parse(await fs.readFile("./cache.json"));
-
-        if (data[0]["time"] !== cache["time"]) {
-            console.log(data[0]["time"] + " // " + cache["time"]) 
-            await fs.writeFile("cache.json", `{ "time" : "${data[0]["time"]}" }`);
-            const guilds = client.guilds.filter(guild => { return client.provider.get(guild, "alerts") });
-            
-            for (let guild of guilds) {
-                let settings = await client.provider.get(guild[1], "alerts");
-                let eqs = data[0]["eqs"].filter(item => { return settings["ships"].includes(item["ship"]) });
-                let format = [];
-                
-                if (!client.channels.get(settings['channel'])) continue;
-                let channel = client.channels.get(settings['channel']);
-
-                if (eqs.length <= 0) continue;
-                if (eqs.length > 0 && eqs.length !== 10) {
-                    for (let eq of eqs) {
-                        format.push(`\`SHIP ${eq['ship']}:\` ${eq['name']} (${eq['jpName']})`);
-                    }
-                }
-                else {
-                    format.push(`\`ALL SHIPS:\` ${eqs[0]['name']} (${eqs[0]['jpName']})`);
-                }
-
-                let time = moment(data[0]["when"]);
-                let string = `:watch:**IN 40 MINUTES:**\n${format.join('\n')}`;
-                
-                if (channel.type == "text" && channel.permissionsFor(client.user).has("SEND_MESSAGES") && channel.permissionsFor(client.user).has("READ_MESSAGES") && guild[1].available) {
-                    try {
-                        await client.channels.get(settings['channel']).send(string);
-                    } catch (err) {
-                        continue;
-                    }
-                }
-            }
-        }
-    } catch (err) {
-        console.error(err);
-    }
-}, 25000, client);
+client.setInterval(handleEQs, 25000, client);
 
 client.login(config.token);
---
+++ eqs.js
@@ -0,0 +1,50 @@
+const fetch = require('node-fetch');
+const fs = require('mz/fs');
+const moment = require('moment');
+
+module.exports = async (client) => {
+    try {
+        const response = await fetch('http://pso2.kaze.rip/eq/');
+        if (response.status !== 200) return;
+
+        const data = await response.json();
+        const cache = JSON.parse(await fs.readFile("./cache.json"));
+
+        if (data[0]["time"] !== cache["time"]) {
+            await fs.writeFile("cache.json", `{ "time" : "${data[0]["time"]}" }`);
+            const guilds = client.guilds.filter(guild => { return client.provider.get(guild, "alerts") });
+            
+            for (let guild of guilds) {
+                let settings = await client.provider.get(guild[1], "alerts");
+                let eqs = data[0]["eqs"].filter(item => { return settings["ships"].includes(item["ship"]) });
+                let format = [];
+                
+                if (!client.channels.get(settings['channel'])) continue;
+                let channel = client.channels.get(settings['channel']);
+
+                if (eqs.length <= 0) continue;
+                if (eqs.length > 0 && eqs.length !== 10) {
+                    for (let eq of eqs) {
+                        format.push(`\`SHIP ${eq['ship']}:\` ${eq['name']} (${eq['jpName']})`);
+                    }
+                }
+                else {
+                    format.push(`\`ALL SHIPS:\` ${eqs[0]['name']} (${eqs[0]['jpName']})`);
+                }
+
+                let time = moment(data[0]["when"]);
+                let string = `:watch:**IN 40 MINUTES:**\n${format.join('\n')}`;
+                
+                if (channel.type == "text" && channel.permissionsFor(client.user).has("SEND_MESSAGES") && channel.permissionsFor(client.user).has("READ_MESSAGES") && guild[1].available) {
+                    try {
+                        await client.channels.get(settings['channel']).send(string);
+                    } catch (err) {
+                        continue;
+                    }
+                }
+            }
+        }
+    } catch (err) {
+        console.error(err);
+    }
+}
