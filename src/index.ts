20141f67a9c08c9b8d72808e623885b464be6d75
Kxze <leite@rodrigo.li>
2018-07-06 21:32:24 -0300

    Initial rewrite commit

---
+++ src/index.ts
@@ -0,0 +1,50 @@
+import * as Discord from "discord.js";
+import * as fs from "fs";
+import * as path from "path";
+import knex from "knex";
+import * as winston from "winston";
+import { IConfig, IModuleParams } from "./types";
+
+const client = new Discord.Client();
+const config: IConfig = JSON.parse(fs.readFileSync(path.join(__dirname, "../config.json"), "utf8"));
+const db = knex(config.database);
+const logger = winston.createLogger({
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.printf(info => {
+      return `[${info.timestamp}] ${info.level}: ${info.message}`;
+    })
+  ),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: "WeebBot.log" })
+  ]
+});
+
+client.on("ready", () => {
+  logger.info("Client ready");
+});
+
+logger.info("Starting bot...");
+// Load modules
+const moduleParameters: IModuleParams = {
+  client,
+  config,
+  db,
+  logger
+};
+
+const modulesDir = path.join(__dirname, "modules");
+const modules = fs.readdirSync(modulesDir)
+  .filter((file: string) => file.endsWith(".js"));
+
+modules.forEach(module => {
+  try {
+    require(path.join(modulesDir, module)).default(moduleParameters);
+    logger.info("Loaded module " + module);
+  } catch (err) {
+    logger.error("Could not load module" + module);
+  }
+});
+
+client.login(config.token);
---
+++ src/modules/eqs.ts
@@ -0,0 +1,81 @@
+import { IModuleParams, IEQData, IDBChannel, IEQ } from "../types";
+import fetch from "node-fetch";
+import * as fs from "fs";
+import * as path from "path";
+import { promisify } from "util"
+import * as Discord from "discord.js";
+
+export default ({ client, config, db, logger }: IModuleParams) => {
+  const readFile = promisify(fs.readFile);
+  const writeFile = promisify(fs.writeFile);
+
+  const cachePath = path.join(__dirname, "../../", "cache.json");
+
+  const fetchApi = async () => {
+    const response = await fetch(config.apiUrl);
+    if (response.status === 200) {
+      const data = await response.json();
+      return data;
+    } else {
+      throw new Error("Could not fetch API. Status code: " + response.status);
+    }
+  }
+
+  const readCache = async () => {
+    const cacheData = JSON.parse(await readFile(cachePath, "utf8"));
+    return cacheData;
+  }
+
+  const buildMessage = (eqs: IEQ[], ships: number[]) => {
+    const embed = new Discord.RichEmbed();
+    embed.setAuthor("PSO2 Emergency Quest Alert", "https://images.emojiterra.com/mozilla/512px/231a.png");
+    embed.setColor("GREEN");
+    embed.setFooter("Help: https://bit.ly/2KRb1De");
+
+    eqs
+      .filter(eq => ships.includes(eq.ship))
+      .forEach(eq => {
+        embed.addField(`Ship ${eq.ship}`, eq.name, true);
+      });
+
+    return embed;
+  }
+
+  const updateCache = async (eq: IEQData) => {
+    const cachePath = path.join(__dirname, "../../", "cache.json");
+    await writeFile(cachePath, JSON.stringify(eq));
+  };
+  
+  const eqModule = async () => {
+    const eqData: IEQData[] = await fetchApi();
+    const lastCachedEq = await readCache();
+    const lastEq = eqData[0];
+    const allChannels: IDBChannel[] = await db("alerts")
+      .select("*");
+
+    // Alert has already been sent
+    if (lastEq.time === lastCachedEq.time) { return; }
+
+    logger.info("New EQ!");
+    await Promise.all(allChannels.map(async (channel) => {
+      const ships = channel.ships.split(",").map(Number); // Oh god why
+      const guild = client.guilds.find(guild => guild.id === channel.guildId);
+      const alertChannel = guild.channels.find(guildChannel => guildChannel.id === channel.channelId) as Discord.TextChannel;
+
+      if (!guild || !alertChannel) { return; }
+
+      const embed = buildMessage(lastEq.eqs, ships);
+
+      try {
+        logger.info(`Sending alert to channel ${alertChannel.name} from guild ${guild.id} with ships ${ships}`);
+        await alertChannel.send(undefined, embed);
+      } catch (err) {
+        // :D?
+      }
+    }));
+
+    await updateCache(lastEq);
+  }
+  
+  client.setInterval(eqModule, 3000);
+}
