b884c504890b16685c52212048e0d4e0d61c8e43
Rodrigo Leite Fernandes <rodrigo@leite.dev>
2021-03-31 20:14:29 -0300

    Change prod URL

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :admin, AdminWeb.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "sleepy-waters-11533.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "weebbot.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
